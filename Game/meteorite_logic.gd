7e6877899472781cbee65da3cb2d65bb7db2a564
tobiasf <30748375+tobiasfrck@users.noreply.github.com>
2024-11-09 16:24:00 +0100

    new spawning

--- Game/meteorite_logic.gd
+++ Game/meteorite_logic.gd
@@ -1,8 +1,8 @@
 extends RigidBody2D
 
-var speed = 40
+var speed = 300
 var startPos = 0
-var direction = Vector2(0,0)
+var move_direction = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	startPos = position
@@ -11,7 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	linear_velocity = direction * delta
+	linear_velocity = move_direction.normalized() * speed * delta
+	move_and_collide(linear_velocity)
 	if position.distance_to(startPos) > 2500:
 		queue_free()
 	pass
